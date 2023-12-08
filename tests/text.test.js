26c52eec09210494f2bbaf8ce3f6a1e72e90baf5
Kowther Kahin <kowtherkahin@Kowthers-MacBook-Pro.local>
2023-12-08 00:31:58 -0800

    adds the vedos ans the genrated svg file

---
+++ tests/text.test.js
@@ -0,0 +1,50 @@
+const { Text } = require("../lib/textClass");
+
+describe("Text", () => {
+  // accurate tests
+  describe("create a black text", () => {
+    it("should create a text with the specified color and chars", () => {
+      const text = new Text("OOO", "black");
+      expect(text.render()).toEqual(
+        `<text x="150" y="150" font-size="50" text-anchor="middle" fill="black">OOO</text>`
+      );
+    });
+  });
+
+  describe("create a pink text", () => {
+    it("should create a text with the specified color and chars", () => {
+      const text = new Text("LPO", "pink");
+      expect(text.render()).toEqual(
+        `<text x="150" y="150" font-size="50" text-anchor="middle" fill="pink">LPO</text>`
+      );
+    });
+  });
+
+  describe("create a blue text", () => {
+    it("should create a text with the specified color and chars", () => {
+      const text = new Text("VJS", "blue");
+      expect(text.render()).toEqual(
+        `<text x="150" y="150" font-size="50" text-anchor="middle" fill="blue">VJS</text>`
+      );
+    });
+  });
+
+  // negative tests
+  describe("create a blue text", () => {
+    it("should create a text with the specified color and chars", () => {
+      const text = new Text("VJS", "white");
+      expect(text.render()).not.toEqual(
+        `<text x="150" y="150" font-size="50" text-anchor="middle" fill="blue">VJS</text>`
+      );
+    });
+  });
+
+  describe("create a blue text", () => {
+    it("should create a text with the specified color and chars", () => {
+      const text = new Text("LOV", "green");
+      expect(text.render()).not.toEqual(
+        `<text x="150" y="150" font-size="50" text-anchor="middle" fill="blue">VJS</text>`
+      );
+    });
+  });
+})
